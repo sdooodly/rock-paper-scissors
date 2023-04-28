0370bc12253daf46bce7530549c3c740c54ed4ed
sdooodly <gayathrigs96@gmail.com>
2023-04-29 01:03:04 +0530

    Base setttttt :D

--- script.js
+++ script.js
@@ -1,45 +1,65 @@
+window.onload = function() {
+  document.getElementById("popup").style.display = "block";
+  
+  const yesBtn = document.getElementById("yesBtn");
+  yesBtn.onclick = function() {
+      document.getElementById("popup").style.display = "none";
+  };
+  
+  const noBtn = document.getElementById("noBtn");
+  noBtn.onclick = function() {
+      alert("Haha psych! Click 'Yes' please! (;");
+  };
+};
+
+
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 const result = document.getElementById("result");
 
 // Function to generate computer's move
-function computerPlay() {
-  const moves = ["rock", "paper", "scissors"];
-  const randomIndex = Math.floor(Math.random() * 3);
-  return moves[randomIndex];
+function getComputerChoice() {
+  // To generate number b/w 0-2
+  const randomNumber = Math.floor(Math.random() * 3)
+  const returned = randomNumber === 0 ? 'rock' : randomNumber === 1 ? 'paper' : 'scissors'
+  return returned
 }
 
-// Function to determine winner
+// Function to determine the winner of each round
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    return "You win!";
+  if (playerSelection.toLowerCase() === computerSelection) {
+    return 'It is a tie! Badamtush!'
+  } else if (playerSelection.toLowerCase() === 'rock') {
+    return computerSelection === 'scissors'
+      ? 'Rock beats scissors, you win!'
+      : 'Paper beats rock, you lose!'
+  } else if (playerSelection.toLowerCase() === 'paper') {
+    return computerSelection === 'rock'
+      ? 'Paper beats rock, you win!'
+      : 'Scissors beat paper, you lose!'
   } else {
-    return "Computer wins!";
+    return computerSelection === 'rock'
+      ? 'Rock beats scissors, you lose!'
+      : 'Scissors beat paper, you win!'
   }
 }
 
 // Event listeners for the buttons
 rockButton.addEventListener("click", () => {
-  const computerSelection = computerPlay();
+  const computerSelection = getComputerChoice();
   const resultText = playRound("rock", computerSelection);
   result.textContent = resultText;
 });
 
 paperButton.addEventListener("click", () => {
-  const computerSelection = computerPlay();
+  const computerSelection = getComputerChoice();
   const resultText = playRound("paper", computerSelection);
   result.textContent = resultText;
 });
 
 scissorsButton.addEventListener("click", () => {
-  const computerSelection = computerPlay();
+  const computerSelection = getComputerChoice();
   const resultText = playRound("scissors", computerSelection);
   result.textContent = resultText;
 });
