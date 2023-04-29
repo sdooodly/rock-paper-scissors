2bcf883e742d082e7083aa927caa55e0123b7f23
sdooodly <gayathrigs96@gmail.com>
2023-04-29 22:07:54 +0530

    Add point system

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
-window.onload = function() {
+window.onload = function () {
   document.getElementById("popup").style.display = "block";
-  
+
   const yesBtn = document.getElementById("yesBtn");
-  yesBtn.onclick = function() {
-      document.getElementById("popup").style.display = "none";
+  yesBtn.onclick = function () {
+    document.getElementById("popup").style.display = "none";
   };
-  
+
   const noBtn = document.getElementById("noBtn");
-  noBtn.onclick = function() {
-      alert("Haha psych! Click 'Yes' please! (;");
+  noBtn.onclick = function () {
+    alert("Haha psych! Click 'Yes' please! (;");
   };
 };
-
 
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 const result = document.getElementById("result");
+const yourScore = document.getElementById("yourScoreCell");
+const computerScore = document.getElementById("computerScoreCell");
+const yct = document.getElementById("ycText");
+const cct = document.getElementById("ccText");
+let ys = 0;
+let cs = 0;
 
 // Function to generate computer's move
 function getComputerChoice() {
   // To generate number b/w 0-2
-  const randomNumber = Math.floor(Math.random() * 3)
-  const returned = randomNumber === 0 ? 'rock' : randomNumber === 1 ? 'paper' : 'scissors'
-  return returned
+  const randomNumber = Math.floor(Math.random() * 3);
+  const returned =
+    randomNumber === 0 ? "rock" : randomNumber === 1 ? "paper" : "scissors";
+  return returned;
 }
 
 // Function to determine the winner of each round
 function playRound(playerSelection, computerSelection) {
   if (playerSelection.toLowerCase() === computerSelection) {
-    return 'It is a tie! Badamtush!'
-  } else if (playerSelection.toLowerCase() === 'rock') {
-    return computerSelection === 'scissors'
-      ? 'Rock beats scissors, you win!'
-      : 'Paper beats rock, you lose!'
-  } else if (playerSelection.toLowerCase() === 'paper') {
-    return computerSelection === 'rock'
-      ? 'Paper beats rock, you win!'
-      : 'Scissors beat paper, you lose!'
+    return "It is a tie! Badamtush!";
+  } else if (playerSelection.toLowerCase() === "rock") {
+    if (computerSelection === "scissors") {
+      ys++;
+      return "Rock beats scissors, you win!";
+    } else {
+      cs++;
+      return "Paper beats rock, you lose!";
+    }
+  } else if (playerSelection.toLowerCase() === "paper") {
+    if (computerSelection === "rock") {
+      ys++;
+      return "Paper beats rock, you win!";
+    } else {
+      cs++;
+      return "Scissors beat paper, you lose!";
+    }
   } else {
-    return computerSelection === 'rock'
-      ? 'Rock beats scissors, you lose!'
-      : 'Scissors beat paper, you win!'
+    if (computerSelection === "rock") {
+      cs++;
+      return "Rock beats scissors, you lose!";
+    } else {
+      ys++;
+      return "Scissors beat paper, you win!";
+    }
   }
 }
 
@@ -49,17 +67,29 @@ function playRound(playerSelection, computerSelection) {
 rockButton.addEventListener("click", () => {
   const computerSelection = getComputerChoice();
   const resultText = playRound("rock", computerSelection);
+  yct.textContent = "Your choice: rock";
+  cct.textContent = `Computer's choice: ${computerSelection}`;
   result.textContent = resultText;
+  yourScore.textContent = ys;
+  computerScore.textContent = cs;
 });
 
 paperButton.addEventListener("click", () => {
   const computerSelection = getComputerChoice();
   const resultText = playRound("paper", computerSelection);
+  yct.textContent = "Your choice: paper";
+  cct.textContent = `Computer's choice: ${computerSelection}`;
   result.textContent = resultText;
+  yourScore.textContent = ys;
+  computerScore.textContent = cs;
 });
 
 scissorsButton.addEventListener("click", () => {
   const computerSelection = getComputerChoice();
   const resultText = playRound("scissors", computerSelection);
+  yct.textContent = "Your choice: scissors";
+  cct.textContent = `Computer's choice: ${computerSelection}`;
   result.textContent = resultText;
+  yourScore.textContent = ys;
+  computerScore.textContent = cs;
 });
