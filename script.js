36706e46f686bce5bca3f2609e5e6e00f7aec1fe
sdooodly <gayathrigs96@gmail.com>
2023-05-07 22:36:16 +0530

    Fixes

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ window.onload = () => {
     };
 };
 
-
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
